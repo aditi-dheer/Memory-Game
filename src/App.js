6143a5eb37a634a859ee6573a701a458dfa5ae5b
aditi-dheer <aditi02@bu.edu>
2025-05-19 18:15:48 +0530

    Added card shuffle functionality and displayed cards in grid layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/assets/helmet-1.png" },
+  { "src": "/assets/potion-1.png" },
+  { "src": "/assets/ring-1.png" },
+  { "src": "/assets/scroll-1.png" },
+  { "src": "/assets/shield-1.png" },
+  { "src": "/assets/sword-1.png" }
+]
+
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  // shuffle cards
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+
+      // generate shuffled array using sort
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Game!</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="front of the card" />
+              <img className="back" src="/assets/cover.png" alt="back of the card" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
+    
+
+
   );
 }
 
